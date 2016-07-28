fc1fcf7cee84e5ea3100661cd85994f11aad2516
Hannah Henderson <hannahc.henderson@gmail.com>
2016-07-28 13:43:28 -0700

    Update chat and remove sublime.project file (#12)
    * Activate handshake between server and client
    
    * Refactor chat to remove extra functions
    
    * Remove unnecessary cod, send msg object
    
    * Remove sublime-project file from repo, update .gitignore
    
    * Remove zombie code

--- src/chat/chatroom.js
+++ src/chat/chatroom.js
@@ -10,6 +10,7 @@ import {
   StyleSheet
 } from 'react-native';
 
+const windowSize = Dimensions.get('window');
 const PULLDOWN_DISTANCE = 40;
 
 export default class Chatroom extends Component {
@@ -19,6 +20,7 @@ export default class Chatroom extends Component {
 
     this.state = {
       message: null,
+      messageList: [],
       location: '37.7837-122.4090',
       demoMode: true,
       userLoggedIn: false,
@@ -27,20 +29,25 @@ export default class Chatroom extends Component {
   }
 
   componentWillMount() {
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.onSendPress = this.onSendPress.bind(this);
+    this.onGetChat = this.onGetChat.bind(this)
   }
 
-  handleInputChange(text) {
-    this.setState({
-      message: text,
-    });
+  onGetChat() {
+    this.props.socket.on('updateMessagesState', updatedChatRoom => {
+      console.log('messages received:', updatedChatRoom);
+      this.setState({ messageList: updatedChatRoom });
+    })
   }
 
-  handleSubmit() {
-    // ADD MESSAGE TO CHAT ROOM
-    props.socket.emit('addMessage', this.message);
-    console.log('addMessage:', this.message);
+  onSendPress() {
+    this.props.socket.emit('addMessageToChatRoom', { 
+        location: this.state.location,
+        message: this.state.message,
+        username: null // TODO: Add Username
+      });
+
+    console.log('addMessageToChatRoom:', this.state.message);
 
     this.setState({
       message: '',
@@ -52,39 +59,35 @@ export default class Chatroom extends Component {
     console.log('navigate back a page');
   }
 
-// DUPLICATES HANDLE SUBMIT
-  onSendPress() {
-    console.log(this.state.message);
-    this.setState({message: ''});
-  }
-
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.topContainer}>
           <TouchableHighlight
-            underlayColor={'blue'}
+            underlayColor={'#dcf4ff'}
             onPress={this.onBackPress}
+            style={{marginLeft: 15}}
           >
-          <Text>Back</Text>
+          <Text style={{color: 'black'}}>&lt; Back</Text>
           </TouchableHighlight>
         </View>
         <View>
-          <Text>CHAT</Text>
+          <Text style={{color: '#fff'}}>CHAT</Text>
         </View>
-        <View>
-          <View style={styles.inputContainer}>
+        <View style={styles.inputContainer}>
+          <View style={styles.textContainer}>
             <TextInput
+              style={styles.input}
               value={this.message}
-              onChangeText={(text) => this.handleInputChange(text)}
+              onChangeText={(text) => this.setState({message: text})}
               />
           </View>
-          <View> 
+          <View style={styles.sendContainer}> 
             <TouchableHighlight
               underlayColor={'red'}
               onPress={() => this.onSendPress()}
               >
-              <Text>SEND</Text>
+              <Text style={styles.sendLabel}>SEND</Text>
             </TouchableHighlight>
           </View> 
         </View>
@@ -102,25 +105,48 @@ var styles = StyleSheet.create({
     backgroundColor: '#ffffff'
   },
   topContainer: {
-    flex: 1,
     flexDirection: 'row',
     justifyContent: 'flex-start',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    paddingTop: 20,
+    backgroundColor: '#c2edff',
+    paddingTop: 1,
+  },
+  chatContainer: {
+    flex: 11,
+    justifyContent: 'center',
+    alignItems: 'stretch'
   },
   inputContainer: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center',
-    backgroundColor: 'blue'
-  }
+    backgroundColor: '#a9e5ff'
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  sendContainer: {
+    justifyContent: 'flex-end',
+    paddingRight: 10
+  },
+  sendLabel: {
+    color: '#ffffff',
+    fontSize: 15
+  },
+  input: {
+    width: windowSize.width - 70,
+    color: '#555555',
+    paddingRight: 10,
+    paddingLeft: 10,
+    paddingTop: 5,
+    height: 32,
+    borderColor: '#6E5BAA',
+    borderWidth: 1,
+    borderRadius: 2,
+    alignSelf: 'center',
+    backgroundColor: '#ffffff'
+  },
 
 });
-
-
-      // <View style={styles.container}>
-      //   <Text>Chatroom Take 2</Text>
-      //   <Text>{this.state.location}</Text>
-      // </View>
