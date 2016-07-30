ecd4e31c6270c1a5c52d2f88ca958a9c8b8ac693
James Ramadan <jamesramadan@gmail.com>
2016-07-29 19:44:35 -0700

    Added in Socket connections to login/signup (#26)
    * Added in Socket connections
    
    * Fixed linting. Made Edits.

--- src/login/login.js
+++ src/login/login.js
@@ -9,69 +9,6 @@ import {
 } from 'react-native';
 import Hyperlink from 'react-native-hyperlink';
 
-export default class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { username: '', password: '' };
-  }
-
-  onButtonPress() {
-    console.log(this.state.username);
-    this.props.navigator.push({
-      name: 'map'
-    })
-  }
-  
-  onLinkPress() {
-    console.log(this.state.username);
-    this.props.navigator.push({
-      name: 'signup'
-    })
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.loginContainer}>
-                  <Image source={require("./cookie.jpg")} />
-                  <Text style={styles.header}> Crumbs </Text>
-                  <TextInput
-                    style={styles.input}
-                    value={this.state.username}
-                    onChangeText={(text) => this.setState({username: text})}
-                    placeholder={'Enter Username'}
-                    maxLength={12}
-                    multiline={false}
-                    />
-                  <TextInput
-                      style={styles.input}
-                      value={this.state.password}
-                      onChangeText={(text) => this.setState({password: text})}
-                      placeholder={'Enter Password'}
-                      maxLength={12}
-                      multiline={false}
-                  />
-                 <TouchableHighlight
-                    style={styles.button}
-                    underlayColor={'#328FE6'}
-                    onPress= {() => this.onButtonPress()}
-                    >
-                    <Text style={styles.label}>LOGIN</Text>
-                </TouchableHighlight>
-                <Hyperlink>
-                            <View>
-                                <Text style={{fontSize:15}}>
-                                Not a member? <Text onPress={() => this.onLinkPress()} style={{color: 'blue', fontSize:15}}>Create a Profile</Text>
-                                </Text>
-                            </View>
-                </Hyperlink>
-        </View>
-      </View>
-    );
-  }
-
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -80,43 +17,115 @@ const styles = StyleSheet.create({
     backgroundColor: '#87CEEB',
   },
   loginContainer: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   header: {
     fontSize: 60,
     color: 'orange',
   },
   input: {
-      width: 250,
-      color: '#555555',
-      padding: 10,
-      height: 50,
-      borderColor: '#32C5E6',
-      borderWidth: 1,
-      borderRadius: 4,
-      alignSelf: 'center',
-      backgroundColor: '#ffffff'
+    width: 250,
+    color: '#555555',
+    padding: 10,
+    height: 50,
+    borderColor: '#32C5E6',
+    borderWidth: 1,
+    borderRadius: 4,
+    alignSelf: 'center',
+    backgroundColor: '#ffffff',
   },
   button: {
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderWidth: 1,
-      borderRadius: 5,
-      borderColor: '#328FE6',
-      padding: 10,
-      marginTop: 10,
-      backgroundColor: '#32c5e6'
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#328FE6',
+    padding: 10,
+    marginTop: 10,
+    backgroundColor: '#32c5e6',
   },
   label: {
-      width: 230,
-      flex: 1,
-      alignSelf: 'center',
-      textAlign: 'center',
-      fontSize: 20,
-      fontWeight: '600',
-      color: '#ffffff'
-  }
+    width: 230,
+    flex: 1,
+    alignSelf: 'center',
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#ffffff',
+  },
 });
 
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: '', password: '' };
+  }
+
+  componentWillMount() {
+    this.props.socket.on('Authentication', username => { 
+    });
+  }
+
+  onLinkPress() {
+    this.props.navigator.push({
+      name: 'signup',
+    });
+  }
+
+  onButtonPress() {
+    this.props.navigator.push({
+      name: 'map',
+    });
+  }
+
+  validateUser() {
+    this.props.socket.emit('validateUserLogin', { 
+      username: this.state.username,
+      password: this.state.password,
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.loginContainer}>
+          <Image source={require("./cookie.jpg")} />
+          <Text style={styles.header}> Crumbs </Text>
+          <TextInput
+            style={styles.input}
+            value={this.state.username}
+            onChangeText={(text) => this.setState({ username: text })}
+            placeholder={'Enter Username'}
+            maxLength={12}
+            multiline={false}
+          />
+          <TextInput
+            style={styles.input}
+            value={this.state.password}
+            onChangeText={(text) => this.setState({ password: text })}
+            placeholder={'Enter Password'}
+            maxLength={12}
+            multiline={false}
+          />
+          <TouchableHighlight
+            style={styles.button}
+            underlayColor={'#328FE6'}
+            onPress= {() => this.validateUser()}
+          >
+            <Text style={styles.label}>LOGIN</Text>
+          </TouchableHighlight>
+          <Hyperlink>
+            <View>
+              <Text style={{ fontSize: 15 }}>
+                Not a member? <Text onPress={() => this.onLinkPress()} style={{color: 'blue', fontSize:15}}>Create a Profile</Text>
+              </Text>
+            </View>
+          </Hyperlink>
+        </View>
+      </View>
+    );
+  }
+}
--- src/login/signup.js
+++ src/login/signup.js
@@ -5,58 +5,7 @@ import {
   StyleSheet,
   TextInput,
   TouchableHighlight,
-  Image
 } from 'react-native';
-import Hyperlink from 'react-native-hyperlink';
-
-export default class Signup extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {username: '', password: ''};
-  }
-
-  onPress() {
-    console.log(this.state.username);
-    this.props.navigator.push({
-      name: 'map'
-    })
-  }
-  
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.loginContainer}>
-                  <Text style={styles.header}> Crumbs </Text>
-                  <Text style={styles.subheader}> Create a Profile </Text>
-                  <TextInput
-                    style={styles.input}
-                    value={this.state.username}
-                    onChangeText={(text) => this.setState({username: text})}
-                    placeholder={'Enter Username'}
-                    maxLength={12}
-                    multiline={false}
-                    />
-                  <TextInput
-                      style={styles.input}
-                      value={this.state.password}
-                      onChangeText={(text) => this.setState({password: text})}
-                      placeholder={'Enter Password'}
-                      maxLength={12}
-                      multiline={false}
-                  />
-                 <TouchableHighlight
-                    style={styles.button}
-                    underlayColor={'#328FE6'}
-                    onPress={() => this.onPress()}
-                    >
-                    <Text style={styles.label}>SIGN UP</Text>
-                </TouchableHighlight>
-        </View>
-      </View>
-    );
-  }
-
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -66,50 +15,109 @@ const styles = StyleSheet.create({
     backgroundColor: '#87CEEB',
   },
   loginContainer: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   header: {
     alignItems: 'center',
     fontSize: 60,
-    color: 'orange'
+    color: 'orange',
   },
   subheader: {
     alignItems: 'center',
     fontSize: 20,
-    color: 'black'
+    color: 'black',
   },
   input: {
-      width: 250,
-      color: '#555555',
-      padding: 10,
-      height: 50,
-      borderColor: '#32C5E6',
-      borderWidth: 1,
-      borderRadius: 4,
-      alignSelf: 'center',
-      backgroundColor: '#ffffff'
+    width: 250,
+    color: '#555555',
+    padding: 10,
+    height: 50,
+    borderColor: '#32C5E6',
+    borderWidth: 1,
+    borderRadius: 4,
+    alignSelf: 'center',
+    backgroundColor: '#ffffff',
   },
   button: {
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderWidth: 1,
-      borderRadius: 5,
-      borderColor: '#328FE6',
-      padding: 10,
-      marginTop: 10,
-      backgroundColor: '#32c5e6'
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#328FE6',
+    padding: 10,
+    marginTop: 10,
+    backgroundColor: '#32c5e6',
   },
   label: {
-      width: 230,
-      flex: 1,
-      alignSelf: 'center',
-      textAlign: 'center',
-      fontSize: 20,
-      fontWeight: '600',
-      color: '#ffffff'
-  }
+    width: 230,
+    flex: 1,
+    alignSelf: 'center',
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#ffffff',
+  },
 });
 
+export default class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: '', password: '' };
+  }
+
+  componentWillMount() {
+    this.props.socket.on('Authentication', username => {
+    });
+  }
+
+  onPress() {
+    this.props.navigator.push({
+      name: 'map',
+    });
+  }
+
+  validateUser() {
+    this.props.socket.emit('validateUserSignup', { 
+      username: this.state.username,
+      password: this.state.password,
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.loginContainer}>
+          <Text style={styles.header}> Crumbs </Text>
+          <Text style={styles.subheader}> Create a Profile </Text>
+          <TextInput
+            style={styles.input}
+            value={this.state.username}
+            onChangeText={(text) => this.setState({ username: text })}
+            placeholder={'Enter Username'}
+            maxLength={12}
+            multiline={false}
+          />
+          <TextInput
+            style={styles.input}
+            value={this.state.password}
+            onChangeText={(text) => this.setState({ password: text })}
+            placeholder={'Enter Password'}
+            maxLength={12}
+            multiline={false}
+          />
+          <TouchableHighlight
+            style={styles.button}
+            underlayColor={'#328FE6'}
+            onPress={() => this.validateUser()}
+          >
+            <Text style={styles.label}>SIGN UP</Text>
+          </TouchableHighlight>
+        </View>
+      </View>
+    );
+  }
+
+}
 
